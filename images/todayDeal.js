648d67e313c7aa99460d9f29e5f8b14379d0dba0
Dhanendra Kumar <142077566+theDhanendra@users.noreply.github.com>
2023-10-07 21:37:10 +0530

    Add files via upload

---
+++ images/todayDeal.js
@@ -0,0 +1,117 @@
+export const todayDeal = [
+    {
+        img : "TD_Oneplus.jpg",
+        discount : 55,
+        DealOfDay : "Deal of the Day",
+        desc : " Oneplus New Launch: Bullets Z2 ANC"
+    },
+    {
+        img : "TD_Bata.jpg",
+        discount : 46,
+        DealOfDay : "Deal of the Day",
+        desc : "Best Styles of Bata, Hush Puppies & More"
+    },
+    {
+        img : "TD_Asian.jpg",
+        discount : 49,
+        DealOfDay : "Deal of the Day",
+        desc : "Starting 299 - ASIAN Footwear Collection"
+    },
+    {
+        img : "TD_FixDerma.jpg",
+        discount : 25,
+        DealOfDay : "Deal of the Day",
+        desc : "Best offers on FixDerma"
+    },
+
+    {
+        img : "TD_Water.jpg",
+        discount : 40,
+        DealOfDay : "Deal of the Day",
+        desc : "WaterScience best deals Bath Faucets..."
+    },
+    {
+        img : "TD_Wallpapers.jpg",
+        discount : 86,
+        DealOfDay : "Deal of the Day",
+        desc : "Wallpapers from wolpin"
+    },
+
+    {
+        img : "TD-tshirt.jpg",
+        discount : 45,
+        DealOfDay : "Deal of the Day",
+        desc : "Casual T-shirts At Best Price"
+    },
+    {
+        img : "TD_Vest.jpg",
+        discount : 60,
+        DealOfDay : "Deal of the Day",
+        desc : "VIP vests at best price ever"
+    },
+
+    // {
+    //     img : "jpg",
+    //     discount : 12,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Handpicked Intel Powered Laptops; High Performance"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/317lhW5iHVL._AC_SY200_.jpg",
+    //     discount : 65,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Powerbank from Mi, Ambrane, URBN and more"
+    // },
+
+    // {
+    //     img : "https://m.media-amazon.com/images/I/41yxd6A+ZAL._AC_SY200_.jpg",
+    //     discount : 65,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Top deals on Truke, Govo and more"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/41wcMkODJLL._AC_SY200_.jpg",
+    //     discount : 75,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Amazing deals on pTron, Jabra, Portronics"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/61QQtY6qtHL._AC_SY200_.jpg",
+    //     discount : 82,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Levi's, Allen Solly, ANNI DESIGNER, Janasya & more"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/31aTPRvXiSL._AC_SY200_.jpg",
+    //     discount : 77,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Exciting  deals on Mivi, Wecool, Wings"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/21wNUazPKRL._AC_SY200_.jpg",
+    //     discount : 20,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Never before deals on HP printers"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/41bFnhrsyOL._AC_SY200_.jpg",
+    //     discount : 19,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Vivo Y75 - 18W fast charge, FHD plus display"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/41wyFlObIrL._AC_SY200_.jpg",
+    //     discount : "25",
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Nokia 5710 - newly launched 4G feature phone"
+    // },
+    // {
+    //     img : "https://m.media-amazon.com/images/I/31Zq-alRH9L._AC_SY200_.jpg",
+    //     discount : 32,
+    //     DealOfDay : "Deal of the Day",
+    //     desc : "Vacuum Cleaners from Top Brands"
+    // },
+
+
+
+]
